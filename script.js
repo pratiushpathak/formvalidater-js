1496fe7191176ec3f320f378a576498a88b62e64
pratiush pathak <pratiushpathak@gmail.com>
2025-04-26 17:25:13 +0530

    added show error function

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 const form=document.getElementById('form');
+const username=document.getElementById('username');
+const email=document.getElementById('email');
+const password=document.getElementById('password');
+const password2=document.getElementById('password2');
+
+//show input error message
+function showError(input,message)
+{
+  const formControl=input.parentElement;
+  formControl.className='form-control error';
+  const small=formControl.querySelector('small');
+  small.innerText=message;
+ 
+
+}
+
+
+
+// event listeners
+form.addEventListener('submit',function(e)
+{
+  e.preventDefault();
+  if(username.value==='')
+  {
+   showError(username,'username is required');
+  }
+  else
+  {
+    showSuccess(username)
+  }
+})
